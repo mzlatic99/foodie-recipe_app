4a41fa9105847c8ca9ed694445b4f033e5fbc370
Mateo Zlatić <cromateozlatic@gmail.com>
2023-05-11 13:02:24 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/foodie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Foodie());
+}
+
+class Foodie extends StatelessWidget {
+  const Foodie({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Foodie Recipe App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
